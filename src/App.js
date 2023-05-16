25bc0e2f7fedf6dbe15d299558f2e34968d5d030
melike1313 <melikenogay13@gmail.com>
2023-05-16 14:27:35 +0300

    gereksinimler commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Sidebar from 'components/Sidebar'
+import BottomBar from 'components/BottomBar'
+import Content from 'components/Content'
+import { BrowserRouter as Router } from "react-router-dom"
+import React, { useState } from "react";
+import './App.css';
+import { Login } from "./Login";
+import { Register } from "./Register";
+
+
+function App() {
+ 
+	return (
+		<Router>
+			<div className="wrapper">
+				<Sidebar/>
+				<Content/>
+			</div>
+			<BottomBar/>
+		</Router>
+		
+		
+	);
+	
+}
+
+
+export default App;

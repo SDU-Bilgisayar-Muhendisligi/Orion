40a901dbb77f08dc0cc415860c2f2341ac40afbb
beyzagursoy <b.gursoy0112@gmail.com>
2023-06-20 15:37:29 +0300

    backend

---
+++ router/userRoute.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var router = express.Router();
+const auth = require("../config/Auth");
+var userController = require("../controller/UserController");
+
+router.post("/kayitol", userController.kayitOl);
+router.post("/girisyap", userController.girisYap);
+
+router
+  .get("/users", userController.getUsers)
+  .get("/users/:userid", userController.getUser)
+  .put("/users/:userid", auth.verifyToken, userController.updateUser)
+  .put("/change-password", auth.verifyToken, userController.changePassword)
+  .get("/me", userController.loggedInUser)
+  .delete("/delete-account", userController.deleteUser);
+
+module.exports = router;
